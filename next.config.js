7bcbdc6b6d76d8dc00c6e273d243933f1717c546
prsabahrami <p.bahraminejad@gmail.com>
2024-09-10 01:44:36 -0400

    fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/prsabahrami.github.io",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
   };
